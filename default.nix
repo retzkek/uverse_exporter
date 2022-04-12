909ce9ff95d5f4be7916cc4978b4dcfc33403d42
Kevin Retzke <retzkek@gmail.com>
2022-04-12 19:03:56 -0400

    fetch source from github

--- default.nix
+++ default.nix
@@ -13,15 +13,15 @@ let
       version = "0.1.0";
 
       # If you have your sources locally, you can specify a path
-      src = /home/kretzke/src/uverse_exporter;
+      #src = /home/kretzke/src/uverse_exporter;
 
       # Pull source from a Git server. Optionally select a specific `ref` (e.g. branch),
       # or `rev` revision hash.
-      #src = builtins.fetchGit {
-      #  url = "git://github.com/stigok/ruterstop.git";
-      #  ref = "master";
-      #  #rev = "a9a4cd60e609ed3471b4b8fac8958d009053260d";
-      #};
+      src = builtins.fetchGit {
+        url = "https://github.com/retzkek/uverse_exporter.git";
+        ref = "main";
+        #rev = "a9a4cd60e609ed3471b4b8fac8958d009053260d";
+      };
 
       # Specify runtime dependencies for the package
       propagatedBuildInputs = [ beautifulsoup4 requests prometheus-client];
